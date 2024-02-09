7f848ee5fb2c7e7e375c554106644c00b81da39b
Arthurs101 <84546401+Arthurs101@users.noreply.github.com>
2024-02-09 17:35:06 -0600

    Fixed multiple issues
    Arreglado AFN a AFD , arreglado minimización, implementado directo de ast - afd , arreglado generación de nodos AFN

--- AFD.py
+++ AFD.py
@@ -1,61 +1,262 @@
+'''Implementacion para conversión a AFD a partir del AFN'''
+from collections import deque
+from AFN import AFNnodo
+from AST import NodoAST
 from graphviz import Digraph
 
 
-class NodoAFD:
-    def init(self, id, estado_final=False):
-        self.id = id
-        self.estado_final = estado_final
+epsi = 'ε'
+
+
+#clase representatnte del nodo
+class AFDNode:
+    contador = 0
+    def __init__(self, subset = set()):
+        self.nombre = f's{AFDNode.contador}'
         self.transiciones = {}
+        self.isAceptacion = False
+        self.subset = subset
+        AFDNode.contador += 1
+
+    def agregar_transicion(self, simbolo, estado_destino):
+        if simbolo in self.transiciones.keys():
+            if estado_destino not in self.transiciones[simbolo]:    
+                self.transiciones[simbolo].append(estado_destino)
+        else:
+            self.transiciones[simbolo] = [estado_destino]
+
+'''metodo que recibe de input los estados finales e iniciales del AFN
+    estado_inical, estado_final nodos AFN
+    simbolos = simbolos del alfabeto para la gramatica
+'''
 
 
-def convertir_arbol_a_afd(arbol):
-    afd = AFD()
-    estado_actual = afd.crear_estado()
-    _convertir_arbol_a_afd(arbol, estado_actual, afd)
-    return afd
+'''obtenner la cerradura epsilon del estado s0'''
+
+def afd_directo(nodo_ast:NodoAST):
+    AFDNode.contador = 0
+    followPosTable = NodoAST.followPos
+    indexDict = NodoAST.posdict
+    States = list() 
+    acceptances = []
+    #obtener el pos final
+    for pos , _set in followPosTable.items():
+        if len(_set) == 0:
+            acceptances.append(pos)
 
 
-def _convertir_arbol_a_afd(arbol, estado_actual, afd):
-    if arbol is None:
+    #crear el primer estado a partir del top en follow pos
+    S0 = AFDNode(followPosTable['1'])
+    if NodoAST.index_pos in S0.subset:
+        S0.isAceptacion = True
+    States.append(S0)
+    
+    for D in States:
+        #recorrer con cada symbolo
+        for symbol , indexes in indexDict.items():
+            U = set()
+            _S = set(indexes)
+
+            #obtener las pos mediante intersección de conjuntos
+            occurences = D.subset & _S 
+            if len(occurences) > 0:
+                for pos in occurences:
+                    #obtetenr todos los follow pos relacionados al simbolo
+                    U = U.union(followPosTable[str(pos)])
+                
+                #verificar si existe
+                if any(state.subset == U for state in States):
+                    for state in States:
+                        if state.subset == U:
+                            if symbol != '#':
+                                D.agregar_transicion(symbol,state)
+                else:
+                    #crear uno nuevo
+                    _U = AFDNode(U)
+                    if NodoAST.index_pos - 1 in U:
+                        _U.isAceptacion = True
+                    States.append(_U)
+                    if symbol != '#':
+                        D.agregar_transicion(symbol,_U)
+
+    return S0
+
+def ceraddura_e(S):
+    e = set()
+    for state in S:
+        e = e.union(_cerradure_e(state))
+    return e
+
+def _cerradure_e(estado:AFNnodo,checked = None):
+    if checked is None:
+        checked = set()
+    if estado in checked:
         return
+    
+    checked.add(estado)
 
-    if isinstance(arbol, str):
-        # Operando
-        afd.agregar_transicion(estado_actual, afd.estado_final, arbol)
-    else:
-        # Operador
-        nuevo_estado = afd.crear_estado()
-        afd.agregar_transicion(estado_actual, nuevo_estado, arbol.valor)
-        _convertir_arbol_a_afd(arbol.izquierda, nuevo_estado, afd)
-        _convertir_arbol_a_afd(arbol.derecha, nuevo_estado, afd)
+    if epsi in estado.transiciones.keys():
+        for next in estado.transiciones[epsi]:
+            _cerradure_e(next,checked)
+    
+    return checked
 
+def get_symbol_transitions(estados, simbolo):
+    visited = set()
+    #obtener los estados con el simbolo 
+    for estado in estados:
+        if simbolo in estado.transiciones.keys():
+            visited = visited.union(estado.transiciones[simbolo])
+    
+    #aplicar cerradura lambda
+    return visited
+        
+def get_simbolos(afn_node: AFNnodo,simbolos_entrada= set(), estados_visitados= []):
+    if afn_node and afn_node.nombre not in estados_visitados:
+        estados_visitados.append(afn_node.nombre)
+        for simbolo in afn_node.transiciones:
+            if simbolo != 'ε' and simbolo not in simbolos_entrada:  # Exclude epsilon transitions
+                simbolos_entrada.add(simbolo)
+            for estado in afn_node.transiciones[simbolo]:
+                get_simbolos(estado,simbolos_entrada,estados_visitados)
+    return simbolos_entrada
 
-class AFD:
-    def init(self):
-        self.estados = {}
-        self.estado_inicial = None
-        self.estado_final = None
+def convertir_afn_a_afd(afn_node_inicial, afn_node_final):
+    AFDNode.contador = 0 #colocar el contador siempre en 0
+    simbolos_entrada = get_simbolos(afn_node_inicial) 
+    estados_afd = []
+    accepted_afd = []
+    s0 = ceraddura_e(set({afn_node_inicial}))
+    S0 = AFDNode(s0)
 
-    def crear_estado(self, estado_final=False):
-        nuevo_estado = NodoAFD(len(self.estados), estado_final)
-        self.estados[nuevo_estado.id] = nuevo_estado
-        if not self.estado_inicial:
-            self.estado_inicial = nuevo_estado
-        if estado_final:
-            self.estado_final = nuevo_estado
-        return nuevo_estado
+    if any(node.nombre == afn_node_final.nombre for node in s0):
+        S0.isAceptacion = True
+    estados_afd.append(S0)
+    
+    for estado in estados_afd:
+        for entrada in simbolos_entrada:
+            subc = ceraddura_e(get_symbol_transitions(estado.subset, entrada))
+            
+            if any(set(estado_afd.subset) == set(subc) for estado_afd in estados_afd):
+                for estado_afd in estados_afd:
+                    if set(estado_afd.subset) == set(subc):
+                        estado.agregar_transicion(entrada, estado_afd)
+                        break
+            else:
+                if len(subc) > 0:
+                    new_subc = AFDNode(subc)
+                    estado.agregar_transicion(entrada, new_subc)
+                    if any(node.nombre == afn_node_final.nombre for node in subc):
+                        new_subc.isAceptacion = True
+                        accepted_afd.append(new_subc)
+                    estados_afd.append(new_subc)
 
-    def agregar_transicion(self, estado_origen, estado_destino, etiqueta):
-        transiciones = self.estados[estado_origen.id].transiciones
-        if etiqueta not in transiciones:
-            transiciones[etiqueta] = []
-        transiciones[etiqueta].append(estado_destino)
+    return {"root" : estados_afd[0] , "alfabeto" : simbolos_entrada,"estados":set(estados_afd),"acceptance":set(accepted_afd)}
 
-    def graficar(self):
-        dot = Digraph(format='png')
-        for estado in self.estados.values():
-            dot.node(str(estado.id), label=f"{estado.id}" + ("*" if estado.estado_final else ""))
-            for etiqueta, estados_destino in estado.transiciones.items():
-                for estado_destino in estados_destino:
-                    dot.edge(str(estado.id), str(estado_destino.id), label=etiqueta)
-        return dot
+def renderAfd(root):
+    dot = Digraph(format='png')
+    dot.attr(rankdir='LR')  # Establecer la orientación horizontal
+    visited = set()  # Crear un conjunto para llevar un registro de estados visitados
+    dot.node('_start', shape='point')
+    dot.edge('_start', root.nombre,)
+    _renderAfd(dot, root ,visited)
+    return dot
+
+def _renderAfd(dot, estado , visited):
+    if estado:
+        if estado in visited:
+            return
+
+        visited.add(estado)
+
+        if estado.isAceptacion:
+            dot.node(estado.nombre, label=estado.nombre, shape='doublecircle')
+        else:
+            dot.node(estado.nombre, label=estado.nombre, shape='circle')
+
+        for entrada, destino in estado.transiciones.items():
+            for dest in destino:
+                dot.edge(estado.nombre, dest.nombre, label=entrada)
+
+        for entrada, destino in estado.transiciones.items():
+            for dest in destino:
+                _renderAfd(dot, dest, visited)
+
+def afd_min(afd:dict):
+    #particiones iniciales
+    #p0 : estados de aceptacion , p1: no aceptacion
+    partitions = [afd["acceptance"],afd["estados"]-afd["acceptance"]]
+    
+    isNewpartition = True
+    
+    #REALIZAR PARTICIONES
+    #realizar iteraciones en búsqueda de particion
+    #detener si la nueva particion es igual a la inical
+    while isNewpartition:
+        next_partitions = [] #siguientes particiones 
+        for partition in partitions:
+            next_partition = {} #subgrupos de la siguiente iteracion
+            for state in partition: #hallar transiciones de los estados por token
+                state_transitions = []
+                for token in afd["alfabeto"]:
+                    token_transition = state.transiciones.get(token ,[]) #todos los estados que hay desde el token, si no existe, dar lista vacía
+                    found_grupo = False
+                    #ver a cual grupo pertenece cada estado de transicion
+                    for s in partitions:
+                        for token_s in token_transition:
+                            if token_s in s:
+                                found_grupo = True
+                                state_transitions.append((token, tuple(s))) #añadir como tupla el token y estados de trancision hallados
+                                #se halló el grupo de pertenencia
+                                break
+                        #se halló el grupo, no revisar los siguientes
+                        if found_grupo:
+                            break
+
+                    #no existe el grupo  a trancisionar
+                    if not found_grupo:
+                        state_transitions.append((token, None))
+                #convertir a tupla , donde si mismo es llave y valor, 
+                #esto para las demás iteraciones poder accesarlo como si fuese llave el subconjunto
+                state_transitions = tuple(state_transitions)
+                #agregar el subconjunto
+                next_partition.setdefault(state_transitions, set()).add(state)
+            next_partitions.extend(next_partition.values()) #solo los valores ( el subconjunto es llave y valor)
+        if next_partitions == partitions:
+            break
+        else:
+            partitions = next_partitions
+    
+    #BUSCAR LAS TRANCISIONES PARA CADA ESTADO
+    AFDNode.contador = 0
+
+    state_transitions = {}
+
+    min_states = []
+    min_root = None
+    for sub in partitions:
+        sub_state = AFDNode(sub)
+        
+        #si alguno del subconjunto es aceptacion , todo el conjunto lo es
+        if any(state.isAceptacion for state in sub):
+            sub_state.isAceptacion = True
+
+        #si el inicial está en el conjunto, el conjunto es el inicial
+        if afd["root"] in sub:
+            min_root = sub_state
+
+        min_states.append(sub_state)
+        
+        #relacionar cada elemento con el nuevo
+        for state in sub:
+            state_transitions[state] = sub_state
+
+    #recorrer cada nodo afd para relacionar los elementos de cada uno
+    for state,min_state in state_transitions.items():
+        #revisar cada transición 
+        for token, destination in state.transiciones.items():
+            for dest in destination:
+                min_state.agregar_transicion(token,state_transitions[dest])
+
+    return min_root
+
--- Main.py
+++ Main.py
@@ -1,24 +1,46 @@
-from AST import construir_arbol_postfix, graficar_arbol
+from AST import construir_arbol_postfix, graficar_arbol,obtener_alfabeto,evaluate_tree
 from Posfix import PostFix
-# Asegúrate de que AFD y las funciones relacionadas están definidas correctamente
-from AFD import AFD, convertir_arbol_a_afd
-import os
-
-# Especifica la ruta al ejecutable de Graphviz manualmente
-os.environ["PATH"] += os.pathsep + 'C:/Users/marce/Downloads/windows_10_msbuild_Release_graphviz-9.0.0-win32/Graphviz/bin'
-
-# Realizar las conversiones
+from AFD import afd_directo,renderAfd,convertir_afn_a_afd ,afd_min
+from AFN import createAFNF , renderAfn , evaluateString
+#Realizar las conversiones
 with open("./PostfixData.txt", 'r') as f:
     for i, line in enumerate(f):
         expresion_infix = line.strip()
+        expresion_infix_direct = expresion_infix + '#'
         # Convertir la expresión infix a postfix
+        
         expresion_postfix = PostFix(expresion_infix)
         # Construir el AST a partir de la expresión postfix
-        arbol = construir_arbol_postfix(expresion_postfix)
-
-        # Usar la función convertir_arbol_a_afd para generar el AFD directamente desde el AST
-        afd = convertir_arbol_a_afd(arbol)
         
-        # Graficar y visualizar el AFD generado
-        afd_grafico = afd.graficar()
-        afd_grafico.view(filename=f'AFD_directo_{i}')
+        arbol_root = construir_arbol_postfix(expresion_postfix)
+        d_a = graficar_arbol(arbol_root)
+        d_a.view(filename="AST from regrex")
+ 
+        init,end= createAFNF(nodo=arbol_root)
+        d_anf = renderAfn(init,end)
+        d_anf.view(filename="AFN from regrex")
+
+        afd = convertir_afn_a_afd(init,end)
+        d_afd = renderAfd(afd["root"])
+        d_afd.view(filename="AFD from regrex")
+
+        #afd directo
+        expresion_infix_direct = expresion_infix + '#'
+        expresion_postfix_direct = PostFix(expresion_infix_direct)
+        arbol_root_direct = construir_arbol_postfix(expresion_postfix_direct)
+        
+        d_a_direct = graficar_arbol(arbol_root_direct)
+        d_a_direct.view(filename="AST from regrex for direct method")
+ 
+        evaluate_tree(arbol_root_direct)
+
+        afd_direct = afd_directo(arbol_root_direct)
+        d_afd_direct = renderAfd(afd_direct)
+        d_afd_direct.view(filename="AFD direct from regrex")
+
+        min_dfa = afd_min(afd)
+        d_min_dfa = renderAfd(min_dfa)
+        d_min_dfa.view(filename="AFD MIN from regrex")
+
+        evaluateString(line.strip(),init, end,afd["root"] ,afd["alfabeto"])
+        i+=1
