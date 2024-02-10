70e92c72cf29a6b17f920acebf76b48269b7c60e
AGM54 <134453750+AGM54@users.noreply.github.com>
2024-02-09 18:43:10 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,37 +1,38 @@
-from AST import construir_arbol_postfix, graficar_arbol,obtener_alfabeto,evaluate_tree
+from AST import construir_arbol_postfix, graficar_arbol, obtener_alfabeto, evaluate_tree
 from Posfix import PostFix
-from AFD import afd_directo,renderAfd,convertir_afn_a_afd ,afd_min
-from AFN import createAFNF , renderAfn , evaluateString
-#Realizar las conversiones
-with open("./PostfixData.txt", 'r') as f:
+from AFD import afd_directo, renderAfd, convertir_afn_a_afd, afd_min
+from AFN import createAFNF, renderAfn, evaluateString
+
+# Realizar las conversiones
+with open("./PostfixData.txt", 'r', encoding='utf-8') as f:  
     for i, line in enumerate(f):
         expresion_infix = line.strip()
         expresion_infix_direct = expresion_infix + '#'
         # Convertir la expresión infix a postfix
-        
+
         expresion_postfix = PostFix(expresion_infix)
         # Construir el AST a partir de la expresión postfix
-        
+
         arbol_root = construir_arbol_postfix(expresion_postfix)
         d_a = graficar_arbol(arbol_root)
         d_a.view(filename="AST from regrex")
- 
-        init,end= createAFNF(nodo=arbol_root)
-        d_anf = renderAfn(init,end)
+
+        init, end = createAFNF(nodo=arbol_root)
+        d_anf = renderAfn(init, end)
         d_anf.view(filename="AFN from regrex")
 
-        afd = convertir_afn_a_afd(init,end)
+        afd = convertir_afn_a_afd(init, end)
         d_afd = renderAfd(afd["root"])
         d_afd.view(filename="AFD from regrex")
 
-        #afd directo
+        # afd directo
         expresion_infix_direct = expresion_infix + '#'
         expresion_postfix_direct = PostFix(expresion_infix_direct)
         arbol_root_direct = construir_arbol_postfix(expresion_postfix_direct)
-        
+
         d_a_direct = graficar_arbol(arbol_root_direct)
         d_a_direct.view(filename="AST from regrex for direct method")
- 
+
         evaluate_tree(arbol_root_direct)
 
         afd_direct = afd_directo(arbol_root_direct)
@@ -42,5 +43,5 @@ with open("./PostfixData.txt", 'r') as f:
         d_min_dfa = renderAfd(min_dfa)
         d_min_dfa.view(filename="AFD MIN from regrex")
 
-        evaluateString(line.strip(),init, end,afd["root"] ,afd["alfabeto"])
-        i+=1
+        evaluateString(line.strip(), init, end, afd["root"], afd["alfabeto"])
+
